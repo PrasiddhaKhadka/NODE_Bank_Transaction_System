6804c1cdef866028426da207409e3483df5e477e
PrasiddhaKhadka <prasiddhak77@gmail.com>
2026-02-12 11:41:03 +0545

    Add: Account Controllers and routes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const authRouter = require('../src/routes/auth.routes')
 const notFoundMiddleware = require('./middlewares/not_found.middleware')
 const errorHandlerMiddleware = require('./middlewares/error.middleware')
 const cookieParser = require('cookie-parser')
+const accountRouter = require('./routes/account.routes')
 
 
 app.use(express.json())
@@ -13,7 +14,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 app.use('/api/v1/auth',authRouter)
-
+app.use('/api/v1/account',accountRouter)
 
 app.use(errorHandlerMiddleware)
 app.use(notFoundMiddleware)
---
+++ src/routes/account.routes.js
@@ -0,0 +1,9 @@
+const express = require("express")
+
+
+const accountRouter = express.Router()
+
+
+
+
+module.exports = accountRouter
